99d0f37ae1910c778979d031fe1845fae2162d0a
angel <nopaixx@gmail.com>
2019-06-09 00:14:17 +0200

    strings ok with tab and return

--- mlpython/mlpython/actors/__init__.py
+++ mlpython/mlpython/actors/__init__.py
@@ -87,14 +87,14 @@ class Actor(db.Model):
                 'Python Module-Python Script',
                 'V1',
                 'V1',
-                'def %ID(input_1, input_2, input_3, input_4, input_5):\
-                    output_1 = None\
-                    output_2 = None\
-                    output_3 = None\
-                    output_4 = None\
-                    output_5 = None\
-                    return output_1, output_2, output_3, output_4, output_5\
-                ',
+                """def %ID(input_1=None, input_2=None, input_3=None, input_4=None, input_5=None):
+    output_1 = None
+    output_2 = None
+    output_3 = None
+    output_4 = None
+    output_5 = None
+    return output_1, output_2, output_3, output_4, output_5
+                """,
                 '',
                 5,
                 5
@@ -104,14 +104,14 @@ class Actor(db.Model):
                 'Data Transformation-Sample and Split-Basic Split Data',
                 'V1',
                 'V1',
-                'def %ID(input1):\
-                    left, right = train_test_split(\
-                    input1, test_size=%ID_%P1, random_state=%ID_%P2)\
-                    return left, right\
-                ',
-                'import numpy as np\
-                 from sklearn.model_selection import train_test_split\
-                ',
+                """def %ID(input1=None):
+    left, right = train_test_split(
+    input1, test_size=%ID_%P1, random_state=%ID_%P2)
+    return left, right
+                """,
+                """import numpy as np
+from sklearn.model_selection import train_test_split
+                """,
                 1,
                 2)
 
