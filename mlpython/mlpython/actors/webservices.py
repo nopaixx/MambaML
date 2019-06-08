cbc88fe0fce7f33a0a7c6fc6488ccd683b9f15f6
angel <nopaixx@gmail.com>
2019-06-08 20:48:22 +0200

    actors_all

--- mlpython/mlpython/actors/webservices.py
+++ mlpython/mlpython/actors/webservices.py
@@ -4,6 +4,7 @@ from ..oauth import provider
 from flask_restful import abort, Resource
 from ..projects import Project
 from ..users import User
+from flask import request
 import requests
 import sys
 
@@ -14,7 +15,7 @@ def allactors():
         print(userLogged.username,"create project")
         if not userLogged:
                 return '', 401
-        frontendVersion = request.form.get('frmntendVersion')
+        frontendVersion = request.form.get('frontendVersion')
         backendVersion = request.form.get('backendVersion')
         actors = Actor.query.filter_by(Actor.frontendVersion==frontendVersion,
                                     Actor.backendVersion==backendVersion).all()
