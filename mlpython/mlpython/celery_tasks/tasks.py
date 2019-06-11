52e66776cba1fe1eb92db0276461c7901a6e4be1
angel <nopaixx@gmail.com>
2019-06-11 22:23:35 +0200

    DINAMIC CODE

--- mlpython/mlpython/celery_tasks/tasks.py
+++ mlpython/mlpython/celery_tasks/tasks.py
@@ -4,3 +4,24 @@ from mlpython.app import celery
 @celery.task
 def add_together(x, y):
     return x + y
+
+
+@celery.task
+def run_str_code(inputs):
+    global ret
+    ret = []
+
+    LOC = """
+import pandas as pd
+def funcion_cliente_str(num): 
+    print(num[0])
+    return num
+real_input = []
+for x in inputs:
+    real_input.append(pd.read_json(x))
+tmp_ret = funcion_cliente_str(real_input) 
+for x in tmp_ret:
+    ret.append(x.to_json())
+"""
+    exec(LOC)
+    return ret
--- mlpython/mlpython/test_task/__init__.py
+++ mlpython/mlpython/test_task/__init__.py
@@ -1,15 +1,30 @@
 from ..celery_tasks.tasks import add_together
+from ..celery_tasks.tasks import run_str_code
 from mlpython.app import app, db
 from ..oauth import provider
 from flask_restful import abort, Resource
 from ..users import User
 from flask import request
+from celery import chain
+import pandas as pd
+import numpy as np
 import requests
 import sys
 
 @app.route('/test_task_add', methods=['GET'])
 def task_add():
-    result = add_together.delay(23, 42)
+    data = np.array([['','Col1','Col2'],
+                ['Row1',1,2],
+                ['Row2',3,4]])
+
+    send=pd.DataFrame(data=data[1:,1:],
+                      index=data[1:,0],
+                      columns=data[0,1:])
+    lista = []
+    lista.append(send.to_json())
+    lista.append(send.to_json())
+    result = chain(run_str_code.s(lista), run_str_code.s(), run_str_code.s())()
+#    result = run_str_code.delay(23, 42)
     # result.wait()
     while not result.ready():
         print("NOT READEY")
